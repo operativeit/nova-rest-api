58284719e1ad48f02666698624137230ffd3fbe8
scramatte <scramatte@id.nixus.es>
2024-09-19 21:56:21 +0200

    Commit initial nova tool template

---
+++ src/Http/Middleware/Authorize.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace EomPlus\NovaRestApi\Http\Middleware;
+
+use Laravel\Nova\Nova;
+use EomPlus\NovaRestApi\NovaRestApi;
+
+class Authorize
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request):mixed  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle($request, $next)
+    {
+        $tool = collect(Nova::registeredTools())->first([$this, 'matchesTool']);
+
+        return optional($tool)->authorize($request) ? $next($request) : abort(403);
+    }
+
+    /**
+     * Determine whether this tool belongs to the package.
+     *
+     * @param  \Laravel\Nova\Tool  $tool
+     * @return bool
+     */
+    public function matchesTool($tool)
+    {
+        return $tool instanceof NovaRestApi;
+    }
+}
